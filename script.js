67646a6c186796fc97845fc0cad12ab93b6a424e
bun2023 <138096357+bun2023@users.noreply.github.com>
2023-07-31 17:02:50 +0900

    portfolio
    Webデザイン練習で「インテリアショップ」のカテゴリーページを作成しました。

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+{
+  // スライダー
+  let swiper = new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    // spaceBetween: 30,
+    loop: true,
+    speed: 3000,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  // ハンバーガーメニュー
+
+  let navbar = document.querySelector(".navbar");
+
+  document.querySelector("#menu").onclick = () => {
+    navbar.classList.toggle("active");
+  }
+
+  window.onscroll = () => {
+    navbar.classList.remove("active");
+  }
+
+}
